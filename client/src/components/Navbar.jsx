5103718a1313ebf7f834aed8fe3ed2b02bb3bd39
bakeyed <yuverseat@gmail.com>
2025-02-25 17:24:36 +0530

    make logo look nice..

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,38 @@
 //import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 function Navbar() {
   return (
     <div className="navbar">
       <div className="container">
-        <div>Logo</div>
-        <div className="links">Links</div>
+        <div>
+          <img src={logo}></img>
+        </div>
+        <div className="links">
+          <Link to="/?cat=art" className="link">
+            <h4>Art</h4>
+          </Link>
+          <Link to="/?cat=phil" className="link">
+            <h4>Philosophy</h4>
+          </Link>
+          <Link to="/?cat=tech" className="link">
+            <h4>Tech</h4>
+          </Link>
+          <Link to="/?cat=gen" className="link">
+            <h4>General</h4>
+          </Link>
+          <span>
+            <h4>Lumie</h4>
+          </span>
+          <span>
+            <h4>Logout</h4>
+          </span>
+          <span className="write">
+            <Link to="/write" className="link">
+              <h4>Write</h4>
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
