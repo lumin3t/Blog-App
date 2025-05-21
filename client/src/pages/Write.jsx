a7b8379ee10d3a2fb42549ed07d9e54f5932f7e7
bakeyed <yuverseat@gmail.com>
2025-05-21 23:53:48 +0530

    write pg

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,16 @@
 //import React from "react";
 
 function Write() {
-  return <div>Write</div>;
+  return (
+    <div className="write">
+      <div className="content">Content</div>
+      <input placeholder="title"></input>
+      <div className="menu">
+        <div className="item">item 1</div>
+        <div className="item">item 2</div>
+      </div>
+    </div>
+  );
 }
 
 export default Write;
