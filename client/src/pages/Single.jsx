d91824b054f31e9dd8faae75a93a01efbc993d9f
bakeyed <yuverseat@gmail.com>
2025-05-23 21:03:04 +0530

    Frontend done :D

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -10,24 +10,38 @@ function Single() {
           <img src="https://external-content.duckduckgo.com/iu/?u=https://tse3.mm.bing.net/th?id%3DOIP.U_VJuupQohwnzXcKMztqWgHaEo%26pid%3DApi&f=1&ipt=b3044f9604f0f67dff0b300df3bfa6bfbab445486f06ba01d59fa3710a04fa41&ipo=images"></img>
           <div className="user">
             <img src="https://external-content.duckduckgo.com/iu/?u=https://tse3.mm.bing.net/th?id%3DOIP.U_VJuupQohwnzXcKMztqWgHaEo%26pid%3DApi&f=1&ipt=b3044f9604f0f67dff0b300df3bfa6bfbab445486f06ba01d59fa3710a04fa41&ipo=images"></img>
+            <div className="info">
+              <span>John</span>
+              <p>Posted 2 days ago</p>
+            </div>
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <div className="icons">E</div>
+              </Link>
+              <div className="icons">D</div>
+            </div>
           </div>
-          <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
-          </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <div className="icons">E</div>
-            </Link>
-            <div className="icons">D</div>
-          </div>
-          <h1>lkfy osduiyf sydf lsduf</h1>
+          <h1>What in the test title?!</h1>
           <p>
-            akuyd kasyt dita isdty aksudy iuas65dc76riw79 iw6teyoyq867er
-            yqwnoe76ti
-            qwiue76itufgqwvhjeiutiqfjwgvhjeiu7968qtwfgvejiu87q968wrytefhgvhjiqu8w7
-            q 7qywleup9qowygehq8w7yetgqlwie0qygwenq7wygehqnw7egyqw8 iia7ty
-            dawue706qtfwvhei nbw6 ftevhbawt6
+            o not repeat yourself (DRY), it is better to create a .flexContainer
+            class in CSS, because here we have lots of elements with display:
+            flex, justify-content: center and align-items: center. Also, the
+            effect for the post image doesnt have to be made with pseudoelement,
+            it is easier to do this with just box-shadow. 24 Reply 5 replies
+            @amaurimdsantos 2 years ago Thank you for your videos. Im following
+            your tutorials and I have started looking for a job because Im
+            confident now. Wish me luck. 1 Reply @LuisVargas616 2 years ago oh
+            man I cant believe this is good and wrong in so many ways! Lama Dev
+            doesnt answer any comment or question. The main folder has been
+            updated or replaced with something new. If you follow alongside you
+            will face frustration when connecting the client to the api. 2 Reply
+            @iUniverse 2 years ago If you dont mind, can you make a fullstack
+            tutorial that includes react, redux, and saga? Nonetheless, thank
+            you very much for this great tutorial. 16 Reply @innabulatova4853 1
+            year ago Thank you for this wonderful tutorial. It is a great start!
+            Ive been doing either front-end or back-end projects, but now I see
+            how it all works together. I had a bit of trouble with the database
+            (because I have never learned about it), but pushed through anyways.
           </p>
         </div>
         <Menu />
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,13 +1,63 @@
-//import React from "react";
-
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 function Write() {
+  const [value, setValue] = useState("");
   return (
-    <div className="write">
-      <div className="content">Content</div>
-      <input placeholder="title"></input>
+    <div className="add">
+      <div className="content">
+        <input placeholder="title"></input>
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          ></ReactQuill>
+        </div>
+      </div>
       <div className="menu">
-        <div className="item">item 1</div>
-        <div className="item">item 2</div>
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="file"
+            name=""
+          ></input>
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button> Save as draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value="art" id="art"></input>
+            <label htmlFor="art">Art</label>/
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="phil" id="phil"></input>
+            <label htmlFor="phil">Philosophy</label>{" "}
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="tech" id="tech"></input>
+            <label htmlFor="tech">Tech</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="gen" id="gen"></input>
+            <label htmlFor="gen">General</label>{" "}
+          </div>
+        </div>
       </div>
     </div>
   );
